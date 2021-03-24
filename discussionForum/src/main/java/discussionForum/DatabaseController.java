8aef550c9d7c5ccd0e1ca8390099a43c580797ce
Long Thanh Vu <longtv@gmail.com>
2021-03-24 21:22:00 +0100

    bytte rekkefølge på ting i databaseController

--- discussionForum/src/main/java/discussionForum/DatabaseController.java
+++ discussionForum/src/main/java/discussionForum/DatabaseController.java
@@ -8,9 +8,17 @@ public interface DatabaseController {
 
     DatabaseController db = new SQLController();
 
-    boolean isEmailUsed(String email);
+    User createUser(String firstName, String lastName, String email, String password);
 
     User signIn(String email, String password);
+
+    boolean isEmailUsed(String email);
+
+    Collection<Course> coursesToUser(User user);
+
+    void createFolder(String name, Collection<Folder> subfolders, Collection<Thread> threads, Course course);
+
+    Collection<Folder> getFolders(Course course);
 
     void postThread(String title, String content, User author, LocalDateTime postedTime, Folder folder, Collection<Tag> tags);
 
@@ -26,31 +34,15 @@ public interface DatabaseController {
 
     void viewPost(User user, Post post, LocalDateTime postedTimed);
 
-    void createFolder(String name, Collection<Folder> subfolders, Collection<Thread> threads, Course course);
-
-    User createUser(String firstName, String lastName, String email, String password);
-
     Collection<Thread> getThreads(Folder folder);
 
     Collection<DiscussionPost> getDiscussionPosts (Thread thread);
 
     Collection<Comment> getComments (DiscussionPost discussionPost);
 
-    //"A student searches for posts with a specific keyword “WAL”. 
-    //The return value of this should be a list of ids of posts matching the keyword."
     Collection<Thread> search(String searchWord, Course course);
 
-    //An instructor views statistics for users and how many post they have read and how many
-    //they have created. These should be sorted on highest read posting numbers. The output is
-    //“user name, number of posts read, number of posts created”. You don’t need to order by 
-    //posts created, but the number should be displayed. The result should also include users
-    //which have not read or created posts.
     Collection<Map<String, String>> getStatistics(User user);
-
-
-    Collection<Course> coursesToUser(User user);
-
-    Collection<Folder> getFolders(Course course);
 
 
 
